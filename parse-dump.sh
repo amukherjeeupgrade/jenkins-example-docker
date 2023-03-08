636127e27b03b89b65c80a6777dfeb2b46e1b9fc
Aninda Mukherjee <86279435+amukherjeeupgrade@users.noreply.github.com>
2023-03-08 00:35:02 -0800

    Add files via upload

---
+++ parse-dump.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+heapdump_volume=/dump
+heapdump=${1}
+reports="org.eclipse.mat.api:suspects org.eclipse.mat.api:overview org.eclipse.mat.api:top_components"
+# https://wiki.eclipse.org/MemoryAnalyzer/FAQ
+
+# function usage() {
+#     printf '%s\n' \
+#         "Eclipse MAT in Docker" \
+#         "Parses Java heap dumps in the command line. Reports will be generated" \
+#         "in the same folder as the heap dump file." \
+#         "" \
+#         "Examples :"
+#         "  docker run --mount type=bind,source=\"\$(pwd)\",target=/dump docker-mat heapdump.hprof" \
+#         "  docker run -e JAVA_OPTS='-Xmx4g -Xms4g' --mount type=bind,source=\"\$(pwd)\",target=/dump docker-mat heapdump.hprof" \
+#         ""
+# }
+
+
+# [[ -z "${heapdump}" ]] && { usage; exit 1; }
+# [[ -f "${heapdump_volume}/${heapdump}" ]] && printf "Analyzing %s\n" ${heapdump} || { printf "'${heapdump}' does not exists\n"; exit 1; }
+
+
+java $JAVA_OPTS \
+    -jar /opt/mat/plugins/org.eclipse.equinox.launcher_*.jar \
+    -consolelog \
+    -application org.eclipse.mat.api.parse \
+    ${heapdump_volume}/${heapdump} \
+    ${reports}
